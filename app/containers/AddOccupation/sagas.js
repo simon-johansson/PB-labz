e7f5f9e68f7e51eb03117716c87d90e653a16bf8
Simon Johansson <simon.johansson@screeninteraction.com>
2016-11-23 17:14:11 +0100

    add stuff

--- app/containers/AddOccupation/sagas.js
+++ app/containers/AddOccupation/sagas.js
@@ -5,6 +5,20 @@
 import { takeLatest } from 'redux-saga';
 import { take, call, put, select, fork, cancel } from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'react-router-redux';
+import Fuse from 'fuse.js';
+import yrken from '../../../yrken.json'
+const options = {
+  shouldSort: true,
+  threshold: 0.3,
+  tokenize: true,
+  matchAllTokens: true,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  keys: ['namn'],
+};
+const fuse = new Fuse(yrken, options);
+
 import {
   CHANGE_QUERY,
   CHANGE_LIST_QUERY,
@@ -25,12 +39,18 @@ export function* getOccupations() {
   const query = yield select(selectQuery());
   // console.log(query);
 
-  const requestURL = `https://www.arbetsformedlingen.se/rest/matchning/rest/af/v1/matchning/matchningskriterier?namnfilter=${query}&typer=yrken&typer=yrkesgrupper&typer=yrkesomraden`;
-  const occupations = yield call(request, requestURL);
+  // const requestURL = `https://www.arbetsformedlingen.se/rest/matchning/rest/af/v1/matchning/matchningskriterier?namnfilter=${query}&typer=yrken&typer=yrkesgrupper&typer=yrkesomraden`;
+  // const occupations = yield call(request, requestURL);
 
-  if (!occupations.err) {
+  // if (!occupations.err) {
+  if (true) {
     // console.log(occupations);
-    yield put(occupationsLoaded(occupations.data));
+    // yield put(occupationsLoaded(occupations.data));
+
+    const result = fuse.search(query);
+    // console.log(result);
+    yield put(occupationsLoaded(result));
+
   } else {
     // yield put(jobsLoadingError(jobs.err));
   }
