e778c2b182f03286122287970260036da131f9a7
Simon Johansson <simon.johansson@screeninteraction.com>
2017-03-16 11:10:21 +0100

    Added stuff

--- app/containers/ListPage/index.js
+++ app/containers/ListPage/index.js
@@ -1316,7 +1316,7 @@ export class ListPage extends React.Component {
             <div className={styles.myCompetences} onClick={this.hideMatchingJobs.bind(this)}>
               <img className={styles.okIcon} src={okIcon} />
               <img className={styles.listIcon} src={listIcon} />
-              Justera matchningskriterier {/*({this.props.knownCompetences.size})*/}
+              Mina matchningskriterier {/*({this.props.knownCompetences.size})*/}
               {/*<span className={styles.right + ' glyphicon glyphicon-chevron-right'}></span>*/}
             </div>
             <span
@@ -1457,7 +1457,7 @@ export class ListPage extends React.Component {
                 className={this.props.currentTab === 'match' ? styles.activeButton : ''}
                 onClick={this.setTabState.bind(this, 'match')}
               >
-                Matchande jobb
+                Kompetensmatchning
                 { this.props.hasMatchningJobs && !this.props.loading &&
                   ` (${this.props.matchingJobs.length})`
                 }
